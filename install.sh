617a012fb8cc26bea083726acc4faed47e072be4
vrocha <vrocha>
2020-01-14 08:31:53 -0300

    Inserção de Extra no Arquivo install.sh = Incluir usuário zabbix no grupo docker

--- install.sh
+++ install.sh
@@ -16,3 +16,4 @@ cp -f opt/docker-monitor/docker-monitor_2.sh /opt/docker-monitor/
 cp -f autodiscovery/docker-interfaces.sh /opt/docker-monitor/
 cp -f autodiscovery/docker-discovery.sh /opt/docker-monitor/
 
+usermod -a -G docker zabbix

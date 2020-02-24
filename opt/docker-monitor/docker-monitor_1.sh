d38729e2331cd057b3a3e7701eb560c8396b7dca
Adonai S. Canez <adonaicanez@gmail.com>
2020-02-24 13:37:46 -0300

    Corrigindo o uso das variáveis para usar aspas e não usar chaves

--- opt/docker-monitor/docker-monitor_1.sh
+++ opt/docker-monitor/docker-monitor_1.sh
@@ -13,69 +13,69 @@ mkdir -p "$CONTAINER_DIR"
 mkdir -p "$TEMP_DIR"
 chmod 777 "$TEMP_DIR"
 
-echo 0 > ${SYSTEM_DIR}/run.script
-RUN_SCRIPT=$(cat ${SYSTEM_DIR}/run.script)
+echo 0 > "$SYSTEM_DIR"/run.script
+RUN_SCRIPT=$(cat "$SYSTEM_DIR"/run.script)
 
-ps aux | grep -v grep | grep "/bin/bash ${DIR_INSTALL_DOCKER_MONITOR}/docker-monitor_2.sh" 1>/dev/null 2>/dev/null
+ps aux | grep -v grep | grep "/bin/bash "$DIR_INSTALL_DOCKER_MONITOR"/docker-monitor_2.sh" 1>/dev/null 2>/dev/null
 if [ $? -ne 0 ]
 then
-	/bin/bash ${DIR_INSTALL_DOCKER_MONITOR}/docker-monitor_2.sh & 
+	/bin/bash "$DIR_INSTALL_DOCKER_MONITOR"/docker-monitor_2.sh & 
 	echo "Iniciando o script 2"
 	sleep 6
 fi
 
-while [ ${RUN_SCRIPT} -eq 0 ]
+while [ "$RUN_SCRIPT" -eq 0 ]
 do
 	startTime=$(date +%s)
-	cat ${LISTA_CNTNER_EM_EXEC} > ${TEMP_DIR}/containers_t1.txt
+	cat "$LISTA_CNTNER_EM_EXEC" > "$TEMP_DIR"/containers_t1.txt
 	while read cntner
 	do
-		curl -XGET -s --unix-socket /var/run/docker.sock http:/v1.4/containers/${cntner}/stats?stream=false > ${TEMP_DIR}/cntner.stats
-		cat ${TEMP_DIR}/cntner.stats > ${CONTAINER_DIR}/${cntner}/cntner.stats 2>/dev/null
-		rm -f ${TEMP_DIR}/cntner.stats
+		curl -XGET -s --unix-socket /var/run/docker.sock http:/v1.4/containers/${cntner}/stats?stream=false > "$TEMP_DIR"/cntner.stats
+		cat "$TEMP_DIR"/cntner.stats > "$CONTAINER_DIR"/"$cntner"/cntner.stats 2>/dev/null
+		rm -f "$TEMP_DIR"/cntner.stats
 		
 		#
 		# Percorre os containers pegando a lista de interfaces de rede e gerando as estatisticas de utilização de cada uma delas
 		#
-		cat ${CONTAINER_DIR}/${cntner}/cntner.stats | jq .networks | grep ": {" | sed -n -r 's/.*"(.*)".*/\1/p' > ${TEMP_DIR}/temp_network.txt
+		cat "$CONTAINER_DIR"/"$cntner"/cntner.stats | jq .networks | grep ": {" | sed -n -r 's/.*"(.*)".*/\1/p' > "$TEMP_DIR"/temp_network.txt
 		while read neteth 
 		do
-			RUN_SCRIPT=$(cat ${SYSTEM_DIR}/run.script)
-			if [ ${RUN_SCRIPT} -ne 0 ]
+			RUN_SCRIPT=$(cat "$SYSTEM_DIR"/run.script)
+			if [ "$RUN_SCRIPT" -ne 0 ]
 			then
 				exit 0
 			fi
-			if [ ! -d ${CONTAINER_DIR}/${cntner}/network/${neteth} ]
+			if [ ! -d "$CONTAINER_DIR"/"$cntner"/network/"$neteth" ]
 			then
-				mkdir ${CONTAINER_DIR}/${cntner}/network/${neteth}
+				mkdir "$CONTAINER_DIR"/"$cntner"/network/"$neteth"
 			fi			
-			cat ${CONTAINER_DIR}/${cntner}/cntner.stats | jq .networks.${neteth}.rx_bytes > ${CONTAINER_DIR}/${cntner}/network/${neteth}/rx_bytes
-			cat ${CONTAINER_DIR}/${cntner}/cntner.stats | jq .networks.${neteth}.tx_bytes > ${CONTAINER_DIR}/${cntner}/network/${neteth}/tx_bytes
-		done < ${TEMP_DIR}/temp_network.txt
-		rm -f ${TEMP_DIR}/temp_network.txt
+			cat "$CONTAINER_DIR"/"$cntner"/cntner.stats | jq .networks."$neteth".rx_bytes > "$CONTAINER_DIR"/"$cntner"/network/"$neteth"/rx_bytes
+			cat "$CONTAINER_DIR"/"$cntner"/cntner.stats | jq .networks.$"neteth".tx_bytes > "$CONTAINER_DIR"/"$cntner"/network/"$neteth"/tx_bytes
+		done < "$TEMP_DIR"/temp_network.txt
+		rm -f "$TEMP_DIR"/temp_network.txt
 	
 		#
 		# disponibiliza a informação de inicialização do container
 		#
-		temp1=$(docker inspect --format='{{json .State.StartedAt}}' ${cntner})
-		temp2=$(echo $temp1 | tr -d "\"") 
+		temp1=$(docker inspect --format='{{json .State.StartedAt}}' "$cntner")
+		temp2=$(echo "$temp1" | tr -d "\"") 
 		temp3=$(date --date="$temp2" +%s)
-		echo $temp3 > ${CONTAINER_DIR}/${cntner}/system/startedAt 2>/dev/null
-	done < ${TEMP_DIR}/containers_t1.txt
-	rm -f ${TEMP_DIR}/containers_t1.txt
+		echo "$temp3" > "$CONTAINER_DIR"/"$cntner"/system/startedAt 2>/dev/null
+	done < "$TEMP_DIR"/containers_t1.txt
+	rm -f "$TEMP_DIR"/containers_t1.txt
 
 	finishTime=$(date +%s)
-	timeExecution=$((${finishTime}-${startTime} ))
+	timeExecution=$(("$finishTime"-"$startTime" ))
 
-	if [ $timeExecution -le ${SLEEP_TIME} ]
+	if [ $timeExecution -le "$SLEEP_TIME" ]
 	then
-		sleep $(( ${SLEEP_TIME} - ${timeExecution} ))
+		sleep $(( "$SLEEP_TIME" - "$timeExecution" ))
 	fi
 
-	RUN_SCRIPT=$(cat ${SYSTEM_DIR}/run.script)
+	RUN_SCRIPT=$(cat "$SYSTEM_DIR"/run.script)
 done
-rm -f ${TEMP_DIR}/temp_network.txt
-rm -f ${TEMP_DIR}/containers_t1.txt
-rm -f ${TEMP_DIR}/cntner.stats
+rm -f "$TEMP_DIR"/temp_network.txt
+rm -f "$TEMP_DIR"/containers_t1.txt
+rm -f "$TEMP_DIR"/cntner.stats
 
 exit 0
